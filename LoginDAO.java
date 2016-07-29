792ba59353cd3ca4ae242c82cecdecf24fa615d0
pinkyanakali <bharathianakali@gmail.com>
2016-07-29 20:27:43 +0530

    Add files via upload

---
+++ LoginDAO.java
@@ -0,0 +1,26 @@
+package com.niit.ShoppingCart;
+
+public class LoginDAO {
+	
+
+	public boolean isValidUser(String userID,String password)
+	{
+		if (userID.equals("niit")&& password.equals("niit"))
+		{
+	return true;
+}
+else
+{
+	return false;
+}
+	}
+}
+
+		
+		
+	
+
+
+	
+
+
